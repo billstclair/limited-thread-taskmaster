1eff4b116d9f47e3c3a037e7b95a0d8c2b699929
Bill St. Clair <billstclair@gmail.com>
2011-07-24 16:46:41 -0400

    First commit

---
+++ limited-thread-taskmaster.asd
@@ -0,0 +1,35 @@
+; -*- mode: lisp -*-
+
+(in-package #:cl-user)
+
+(asdf:defsystem :limited-thread-taskmaster
+  :description "A taskmaster for Hunchentoot that limits the number of worker threads."
+  :author "Bill St. Clair <bill@billstclair.com>"
+  :version "1.0"
+  :license "Apache"
+  :depends-on (hunchentoot eager-future)
+  :components
+  ((:module src
+    :serial t
+    :components
+    ((:file "package")
+     (:file "taskmaster")
+     ))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Copyright 2011 Bill St. Clair
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;     http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions
+;;; and limitations under the License.
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ src/taskmaster.lisp
@@ -0,0 +1,87 @@
+; -*- mode: lisp -*-
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; A Hunchentoot taskmaster that limits the number of worker threads
+;;;
+
+(in-package :limited-thread-taskmaster)
+
+(defparameter *hunchentoot-worker-thread-limit* 10)
+
+(defvar *eager-future-pool* nil)
+
+(defun eager-future-pool ()
+  (or *eager-future-pool*
+      (let ((pool (make-instance 'eager-future:fixed-fifo-thread-pool)))
+        (setf (eager-future:thread-limit pool) *hunchentoot-worker-thread-limit*
+              *eager-future-pool* pool))))
+
+(defun limited-thread-limit ()
+  "Return the maximum number of hunchentoot worker threads that will be created."
+  *hunchentoot-worker-thread-limit*)
+
+(defun (setf limited-thread-limit) (limit)
+  "Set the maxmimum number of worker threads that will be created.
+LIMIT must be an integer >= 2."
+  (check-type limit integer)
+  (assert (>= limit 2))
+  (setf *hunchentoot-worker-thread-limit* limit)
+  (let ((pool *eager-future-pool*))
+    (when pool
+      (setf (eager-future:thread-limit pool) limit)))
+  limit)
+
+(defclass limited-thread-taskmaster (hunchentoot:one-thread-per-connection-taskmaster)
+  ((thread-pool :initform (eager-future-pool)
+                :accessor thread-pool-of))
+  (:documentation "A Hunchentoot taskmaster that limits the number of worker threads."))
+
+(defmethod hunchentoot:handle-incoming-connection
+    ((taskmaster limited-thread-taskmaster) socket)
+  (let ((eager-future:*thread-pool* (thread-pool-of taskmaster)))
+    (hunchentoot::handler-case*
+     (eager-future:pexec
+       (let ((thread (bt:current-thread)))
+         (declare (ignorable thread))
+         #+ccl
+         (setf (ccl:process-name thread)
+               (format nil "Hunchentoot worker \(client: ~A)"
+                       (hunchentoot::client-as-string socket)))
+         (unwind-protect
+              (hunchentoot:process-connection
+               (hunchentoot:taskmaster-acceptor taskmaster)
+               socket)
+           #+ccl
+           (setf (ccl:process-name thread) "Hunchentoot worker (idle)"))))
+     (error (cond)
+       ;; need to bind *ACCEPTOR* so that LOG-MESSAGE can do its work.
+       (let ((hunchentoot:*acceptor* (hunchentoot:taskmaster-acceptor taskmaster)))
+         (hunchentoot:log-message
+          hunchentoot:*lisp-errors-log-level*
+          "Error while scheduling new incoming connection: ~A"
+          cond))))))
+
+(defclass limited-thread-acceptor (hunchentoot:acceptor)
+  ()
+  (:default-initargs
+   :taskmaster (make-instance 'limited-thread-taskmaster))
+  (:documentation "A Hunchentoot acceptor that uses a limited-thread-taskmaster to imit the number of worker threads."))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Copyright 2011 Bill St. Clair
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;     http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions
+;;; and limitations under the License.
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
